4a96fcc8aed4155b03e676651459659cbca13361
Demi-Brass99 <austinmatthew1999@gmail.com>
2023-03-29 16:28:31 -0400

    Added Button Functionality and Output text is working

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
 
+var calculationString = "";
+
 window.onload = function() {
 
+    calculationString = "";
     
+}
+
+function clickbutton(value) {
+
+    calculationString += value;
+    document.getElementById("CalculationOutput").value = calculationString;
+
+
+}
+
+function calulateSolution() {
+    console.log(parseInt(calculationString));
+}
+
+function clearCalculator() {
+    calculationString = "";
+    document.getElementById("CalculationOutput").value = calculationString;
 }
 
 
