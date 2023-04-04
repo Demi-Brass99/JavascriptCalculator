5f467526b13430369bb6c0644d5f057978f1722f
Demi-Brass99 <austinmatthew1999@gmail.com>
2023-04-04 09:33:36 -0400

    Added Tutorial Resource, Changed Structure of Calculator in HTML, Finalized Project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Javascript Calculator
 Making a calculator application to learn and better understand javascript
+
+
+### Tutorials Used
+* How To Make A Calculator Using HTML, CSS and Javascript <br> https://www.youtube.com/watch?v=cGgLHJGyS34 
--- script.js
+++ script.js
@@ -1,49 +1,9 @@
 
 
-var calculation = 0;
-var currentSymbol = "";
-
-var symbols = ['+','.','-','/','*'];
-
 window.onload = function() {
-
-    calculation = 0;
-    clearCalculator();
     
 }
 
-function clickbutton(value) {
-
-    var isSymbol = false;
-    for (var i = 0; i < symbols.length; i++) {
-        if (value == symbols[i]) {
-            isSymbol = true;
-            currentSymbol = value;
-        }
-    }
-
-    if(!isSymbol) {
-        calculation *= 10;
-        calculation += value;
-        if(currentSymbol == '.') {
-            calculation /= 10;
-            //currentSymbol = '';
-        }
-    }
-
-    document.getElementById("CalculationOutput").value = calculation;
-
-
-}
-
-function calulateSolution() {
-    console.log(calculation);
-}
-
-function clearCalculator() {
-    calculation = 0;
-    document.getElementById("CalculationOutput").value = 0;
-}
 
 
 
